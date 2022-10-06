1c4ac7621fdf5717149ff7f177827e0bb664683a
Seongbae103 <seongbae103@gmail.com>
2022-10-06 14:18:42 +0900

    10-06 14:18 화폐교환

--- greedy/화폐교환.py
+++ greedy/화폐교환.py
@@ -1,9 +1,9 @@
 '''
- * 금액을 입력받은 후 우리나라 화폐종류별로 해당 갯수를 표기하는 프로그램
-    * [요구사항] 금융업을 하는 고객사로부터 프로그램 개발요청이 들어왔습니다.
-    * 금액을 입력받은 후 우리나라 화폐종류별로 해당 갯수를 표기하는 프로그램입니다.
-    * 예를들어, 125,520 원을 입력하면 화면에 이렇게 보이게 하면 됩니다.
-    * 표시하고 10원미만은 절삭
+* 금액을 입력받은 후 우리나라 화폐종류별로 해당 갯수를 표기하는 프로그램
+* [요구사항] 금융업을 하는 고객사로부터 프로그램 개발요청이 들어왔습니다.
+* 금액을 입력받은 후 우리나라 화폐종류별로 해당 갯수를 표기하는 프로그램입니다.
+* 예를들어, 125,520 원을 입력하면 화면에 이렇게 보이게 하면 됩니다.
+* 표시하고 10원미만은 절삭
       ******************************************************
          요청금액 : 126520 원
          5만원 : 2매
@@ -18,12 +18,21 @@
 '''
 
 class Solution:
-   def solution(self):
-      title = " ### 화폐교환 ###"
-      answer = "요청금액 : 126520 원"
-      aster = "*"*30
-      return f'{title} \n {aster} \n {answer} \n {aster}' 
+   def solution(self, money):
+      title = "### 화폐교환 ###"
+      aster = "*" * 30
+      a = int(money/50000)
+      b = int(money%50000/10000)
+      c = int(money%10000/5000)
+      d = int(money%5000/1000)
+      e = int(money%1000/500)
+      f = int(money%500/100)
+      g = int(money%100/50)
+      h = int(money%50/10)
+      answer = f"요청금액 : {money} \n 5만원 : {a}매 \n 1만원 : {b}매 \n 5천원 : {c}매 \n 1천원 : {d}매 \n 5백원 : {e}매 \n 백원 : {f}매 \n 오십원 : {g}매 \n 십원 : {h}매"
+      return f' {title} \n {aster} \n {answer} \n {aster}'
 
 if __name__=="__main__":
    solution = Solution()
-   print(solution.solution())
+   money = int(input("교환할 금액 : "))
+   print(solution.solution(money))
