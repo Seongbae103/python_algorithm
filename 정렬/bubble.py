e6357c95c906df0cdd82e804a6115e7058297ec2
Seongbae103 <seongbae103@gmail.com>
2022-10-07 18:13:47 +0900

    10-07 18:13 버블

--- 정렬/bubble.py
+++ 정렬/bubble.py
@@ -5,12 +5,13 @@ class Solution:
         keys = ['1st', '2ed', '3rd', '4th', '5th']
         dc = {}
         for i in keys:
-            dc[i] = random.randint(1, 11)
-        print(" ### 난수 ### ")
-        print("*" * 30)
+            dc[i] = random.randint(1, 11)            
 
+        print(" ### 화폐교환 ### ")
+        print("*" * 30)
         for k,v in dc.items():
             print(f' {k} : {v}')
+
         print("*" * 30)
 
 if __name__=="__main__":
